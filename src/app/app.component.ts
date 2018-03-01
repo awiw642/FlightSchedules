024a797875b8181f9637583d0001146f359d85ce
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-02 06:45:52 +1300

    Create get all flights method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { IFlight } from "app/models/flight.interface";
+import { Component, ViewEncapsulation } from '@angular/core';
+import { IFlight } from 'app/models/flight.interface';
+import { FlightsService } from './services/flights.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: [
-    "../assets/css/reset.css",
-    "./app.component.css",
-    "../assets/css/fonts.css"
-  ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['../assets/css/reset.css', './app.component.css', '../assets/css/fonts.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private flightService: FlightsService) { }
   public data: Array<IFlight> = [];
+
+  ngOnInit() {
+    this.getFlights();
+  }
+
+  getFlights() {
+    this.flightService.allFlights()
+      .subscribe(flights => this.data = flights);
+  }
 }
