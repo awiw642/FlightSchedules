29edfc50dab1c5c466a2fb7a29fa949bf2537c1e
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-02 09:45:38 +1300

    Create registrations service

---
+++ src/app/services/registrations.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+const API_URL = 'http://localhost:3000/registrations/';
+const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
+
+@Injectable()
+export class RegistrationsService {
+  constructor(private http: Http) {}
+
+  allRegistrations() {
+    return this.http.get(API_URL).map(res => {
+      console.log('Get All the registrations: ', res.json());
+      return res.json();
+    });
+  }
+}
