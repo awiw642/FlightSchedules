1fa8d3465b1f5d1fae126386dcb012519f118b14
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-02 06:45:11 +1300

    Setup Angular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { DataGridModule } from "./components/data-grid/data-grid.module"
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Http } from '@angular/http';
+import { DataGridModule } from './components/data-grid/data-grid.module';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { FlightsService } from './services/flights.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    DataGridModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, DataGridModule, HttpModule],
+  providers: [FlightsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/data-grid/data-grid.module.ts
+++ src/app/components/data-grid/data-grid.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DataGridComponent } from "./data-grid.component";
-import { GridHeaderModule } from "./header/header.module"
-import { RowModule } from "./row/row.module"
-import { CommonModule } from "@angular/common";
+import { DataGridComponent } from './data-grid.component';
+import { GridHeaderModule } from './header/header.module';
+import { RowModule } from './row/row.module';
+import { CommonModule } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+
+import { FlightsService } from '../../services/flights.service';
 
 @NgModule({
-    declarations: [DataGridComponent],
-    exports: [ DataGridComponent],
-    imports: [ GridHeaderModule, RowModule, CommonModule ]
-
+  declarations: [DataGridComponent],
+  exports: [DataGridComponent],
+  imports: [
+    GridHeaderModule,
+    RowModule,
+    CommonModule,
+    DialogModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
+  providers: [FlightsService],
 })
 export class DataGridModule {}
