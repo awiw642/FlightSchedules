20292bb28a6a8cbf823c568a34689912a3145e1a
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-03-02 06:46:41 +1300

    Consume register flight function

--- src/app/components/data-grid/row/row.component.ts
+++ src/app/components/data-grid/row/row.component.ts
@@ -1,14 +1,13 @@
-import { 
-    Component,
-    Input
-} from "@angular/core";
-import { IFlight } from "app/models/flight.interface";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { IFlight } from 'app/models/flight.interface';
 
 @Component({
-  selector: "data-row",
-  templateUrl: "./row.component.html",
-  styleUrls: ["./row.component.css"]
+  selector: 'data-row',
+  templateUrl: './row.component.html',
+  styleUrls: ['./row.component.css'],
 })
 export class RowComponent {
-    @Input() row: IFlight;
+  @Input() row: IFlight;
+  @Output() register = new EventEmitter();
 }
